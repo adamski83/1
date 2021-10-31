bb7522e5847c37f98bf0f706dc986d73428e2fcc
adamski83 <66271771+adamski83@users.noreply.github.com>
2021-10-31 15:55:33 +0100

    Delete web-server directory

--- web-server/reduce/reduce.js
+++
@@ -1,9 +0,0 @@
-const arr1 = [3, 5, 4, 3, 6, 2, 3, 4, ];
-const arr2 = [...arr1];
-const result = arr1.reduce((acc, cur) => acc + cur, 0);
-console.log(result);
-
-
-const fruits = ['apple', 'banana'];
-const res2 = arr2.push.apply(arr1, fruits);
-console.log(res2);
